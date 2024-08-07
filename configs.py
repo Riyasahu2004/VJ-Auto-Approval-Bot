1ba71731ae5536c33e6c0a48c76366bd558f3b04
Riya Sahu <152384568+Riyasahu2004@users.noreply.github.com>
2024-08-07 15:20:44 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,13 +1,13 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "10471716"))
-    API_HASH = getenv("API_HASH", "f8a1b21a13af154596e2ff5bed164860")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7240311570:AAEzQj2J_sQq7cIPyuGZxIMHpnTANYd37GM")
+    API_ID = int(getenv("API_ID", "22027848"))
+    API_HASH = getenv("API_HASH", "38d38ac476379f7c0a60a1964ffe2eb3)
+    BOT_TOKEN = getenv("BOT_TOKEN", "7184639056:AAELK02xUZeHYtR2pDX24MHp9mZqXc8BHzg")
     FSUB = getenv("FSUB", "")
-    CHID = int(getenv("CHID", ""))
-    SUDO = list(map(int, getenv("SUDO", "6883997969 5460214165").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://appuz:chrijismiappuz@cluster0.yngvhc2.mongodb.net/?retryWrites=true&w=majority")
+    CHID = int(getenv("CHID", "-1001966682921"))
+    SUDO = list(map(int, getenv("SUDO", "1750583099 827547960").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://vg766337:lOFhHGJF4vkSzaOY@cluster0.re8zmhy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
 
